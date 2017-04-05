1fdd697f8b2cd91620600a359efba72a0adee8c4
Tomasz Lipka-Bartosik <tomaszlipkabartosik@gmail.com>
2017-04-05 23:42:10 +0200

    Header done. More or less

---
+++ js/app.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){
+console.log("test");
+
+
+}) //END OF SCRIPT
+
+
+
+
+// jQuery
+
+// $(document).ready(function(){
+//  console.log("test");
+//
+//
+// }) //END OF SCRIPT
