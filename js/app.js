c416dd7442419dcfb5510d0d4f57aa2071c7a6f4
Tomasz Lipka-Bartosik <tomaszlipkabartosik@gmail.com>
2017-04-06 23:29:23 +0200

    Added first section and jquery

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
-document.addEventListener("DOMContentLoaded", function(){
-console.log("test");
+$(document).ready(function(){
+ // variables
+  var s_f_header = $("#s_f_header");
+  console.log(document.body.clientWidth);
+ $(window).scroll(function() {
+   var wScroll = $(this).scrollTop();
+
+   if (wScroll > s_f_header.offset().top - 200) {
+     console.log("hi")
+   }
+
+ });
+
+
+
 
 
 }) //END OF SCRIPT
-
-
-
-
-// jQuery
-
-// $(document).ready(function(){
-//  console.log("test");
-//
-//
-// }) //END OF SCRIPT
