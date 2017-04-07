d6497c94b7abeb04d51347e30896e2403e559874
Tomasz Lipka-Bartosik <tomaszlipkabartosik@gmail.com>
2017-04-07 22:46:15 +0200

    Started making elements of first section land

--- js/app.js
+++ js/app.js
@@ -1,13 +1,29 @@
 $(document).ready(function(){
- // variables
-  var s_f_header = $("#s_f_header");
-  console.log(document.body.clientWidth);
+
+// variables
+  var landHeight = ($(window).height())/2;
+  var sf_header = $(".sf_header");
+  var sf_paragraph = $(".sf_paragraph");
+  var sf_pic = $(".sf_pic");
+
+
+
+// scroll
  $(window).scroll(function() {
    var wScroll = $(this).scrollTop();
 
-   if (wScroll > s_f_header.offset().top - 200) {
-     console.log("hi")
+   if (wScroll >sf_header.offset().top - landHeight) {
+     sf_header.addClass('sfh_land');
    }
+
+   if (wScroll >sf_paragraph.offset().top - landHeight) {
+     sf_paragraph.addClass('sfh_land');
+   }
+
+   if (wScroll >sf_pic.offset().top - landHeight) {
+     sf_pic.addClass('sfh_land');
+   }
+
 
  });
 
